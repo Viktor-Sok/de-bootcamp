82014c5fee71ab09fd6584b85b68df3b491b52b6
VikSok92 <sok92@mail.ru>
2025-01-21 19:56:21 +0300

    commit

---
+++ Module_1:Docker_SQL/Q7/answer.sh
@@ -0,0 +1,3 @@
+terraform init # Downloading the provider plugins and setting up backend
+terraform apply -auto-approve # Generating proposed changes and auto-executing the plan
+terraform destroy # Remove all resources managed by terraform
